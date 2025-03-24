0a2bddc9905345bf0d0e2c071eec3614f10bb43d
Nguyen Huy <nghuy29032004@gmail.com>
2025-03-24 19:52:23 +0700

    add file assets

---
+++ lib/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _Homepage();
+
+}
+
+class _Homepage extends State<HomePage>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold();
+  }
+}
