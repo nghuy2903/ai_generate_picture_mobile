4523048bb3ae7d76bcde6eaf2f165ab02cc4603d
Nguyen Huy <nghuy29032004@gmail.com>
2025-03-25 21:02:47 +0700

    setting enviroment and prepair for speech to text

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:ai_generate_picture/feature_box.dart';
 import 'package:ai_generate_picture/pallete.dart';
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,6 +12,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _Homepage extends State<HomePage>{
+  final speechToText = SpeechToText();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initSpeechToText();
+  }
+
+  Future<void> initSpeechToText() async{
+    await speechToText.initialize();
+  }
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -19,96 +32,98 @@ class _Homepage extends State<HomePage>{
         leading: const Icon(Icons.menu),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          //Virtual Assistants
-          Stack(
-            children: [
-              Center(
-                child: Container(
-                  height: 120,
-                  width: 120,
-                  margin: const EdgeInsets.only(top: 4),
-                  decoration: const BoxDecoration(
-                    color: Pallete.assistantCircleColor,
-                    shape: BoxShape.circle
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //Virtual Assistants
+            Stack(
+              children: [
+                Center(
+                  child: Container(
+                    height: 120,
+                    width: 120,
+                    margin: const EdgeInsets.only(top: 4),
+                    decoration: const BoxDecoration(
+                      color: Pallete.assistantCircleColor,
+                      shape: BoxShape.circle
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 123,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(image: AssetImage("assets/images/virtualAssistant.png"))
+                  ),
+                )
+              ],
+            ),
+        
+            //chat bubble
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
+              ),
+              margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 30).copyWith(
+                top: 30,
+        
+              ),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Pallete.borderColor
+                ),
+                borderRadius: BorderRadius.circular(20).copyWith(
+                  topLeft: Radius.zero,
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: const Text('Hi! What task can I do for you?', 
+                  style: TextStyle(
+                    fontFamily: 'Cera Pro',
+                    color: Pallete.mainFontColor,
+                    fontSize: 25,
                   ),
                 ),
               ),
-              Container(
-                height: 123,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(image: AssetImage("assets/images/virtualAssistant.png"))
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.only(
+                top: 10,
+                left: 22,
+              ),
+              child: const Text('Here are a few features', style: TextStyle(
+                fontFamily: 'Cera Pro',
+                color: Pallete.mainFontColor,
+                fontSize: 20,
+                fontWeight: FontWeight.bold
+              ),),
+            ),
+            //Features List
+            Column(
+              children: [
+                FeatureBox(
+                  color: Pallete.firstSuggestionBoxColor,
+                  headerText: 'Chat GPT',
+                  descriptionText: 'If you dont change it, it mean you accept it',
                 ),
-              )
-            ],
-          ),
-
-          //chat bubble
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 10,
-            ),
-            margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 30).copyWith(
-              top: 30,
-
-            ),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Pallete.borderColor
-              ),
-              borderRadius: BorderRadius.circular(20).copyWith(
-                topLeft: Radius.zero,
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
-              child: const Text('Hi! What task can I do for you?', 
-                style: TextStyle(
-                  fontFamily: 'Cera Pro',
-                  color: Pallete.mainFontColor,
-                  fontSize: 25,
+                FeatureBox(
+                  color: Pallete.secondSuggestionBoxColor,
+                  headerText: 'Dall-E',
+                  descriptionText: 'Get inspired and stay creative with your personal assiatant powered by dall-e',
                 ),
-              ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            alignment: Alignment.centerLeft,
-            margin: const EdgeInsets.only(
-              top: 10,
-              left: 22,
-            ),
-            child: const Text('Here are a few features', style: TextStyle(
-              fontFamily: 'Cera Pro',
-              color: Pallete.mainFontColor,
-              fontSize: 20,
-              fontWeight: FontWeight.bold
-            ),),
-          ),
-          //Features List
-          Column(
-            children: [
-              FeatureBox(
-                color: Pallete.firstSuggestionBoxColor,
-                headerText: 'Chat GPT',
-                descriptionText: 'If you dont change it, it mean you accept it',
-              ),
-              FeatureBox(
-                color: Pallete.secondSuggestionBoxColor,
-                headerText: 'Dall-E',
-                descriptionText: 'Get inspired and stay creative with your personal assiatant powered by dall-e',
-              ),
-              FeatureBox(
-                color: Pallete.thirdSuggestionBoxColor,
-                headerText: 'Small Voice Assistant',
-                descriptionText: 'Get the best of both worlds with a voice assiatant powered by Dall-E and chat GPT',
-              ),
-            ],
-          )
-        ],
+                FeatureBox(
+                  color: Pallete.thirdSuggestionBoxColor,
+                  headerText: 'Small Voice Assistant',
+                  descriptionText: 'Get the best of both worlds with a voice assiatant powered by Dall-E and chat GPT',
+                ),
+              ],
+            )
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Pallete.firstSuggestionBoxColor,
@@ -118,4 +133,5 @@ class _Homepage extends State<HomePage>{
         
     );
   }
+  
 }
