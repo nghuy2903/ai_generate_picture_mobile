2442c521e66dc5bb933b85e509a42c278b12fae5
Nguyen Huy <nghuy29032004@gmail.com>
2025-03-28 22:46:58 +0700

    complete build chat with  gemini

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,179 +1,157 @@
-import 'package:ai_generate_picture/feature_box.dart';
-import 'package:ai_generate_picture/pallete.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:speech_to_text/speech_recognition_result.dart' show SpeechRecognitionResult;
 import 'package:speech_to_text/speech_to_text.dart';
+import 'message.dart';
+import 'pallete.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
   State<StatefulWidget> createState() => _Homepage();
-
 }
 
-class _Homepage extends State<HomePage>{
+class _Homepage extends State<HomePage> {
   final speechToText = SpeechToText();
   String lastWords = '';
+  late GenerativeModel _model;
+  late ChatSession _chatSession;
+  final TextEditingController _textController = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
+  bool _loading = false;
 
   @override
   void initState() {
     super.initState();
-    initSpeechToText();
+    _loadApiKey();
   }
 
-  Future<void> initSpeechToText() async{
-    await speechToText.initialize();
-    setState(() {});
-  }
+  Future<void> _loadApiKey() async {
+    try {
+    // Đọc file env.json từ assets
+    final String jsonString = await rootBundle.loadString('assets/env.json');
+    final Map<String, dynamic> jsonData = json.decode(jsonString);
 
-  /// Each time to start a speech recognition session
-  Future<void> startListening() async {
-    await speechToText.listen(onResult: onSpeechResult);
-    setState(() {});
-  }
+    // Lấy API key từ file JSON
+    String apiKey = jsonData['api_key']?.toString() ?? '';
 
-  /// Manually stop the active speech recognition session
-  /// Note that there are also timeouts that each platform enforces
-  /// and the SpeechToText plugin supports setting timeouts on the
-  /// listen method.
-  Future<void> stopListening() async {
-    await speechToText.stop();
-    setState(() {});
-  }
+    // Kiểm tra API Key hợp lệ
+    if (apiKey.isNotEmpty) {
+      setState(() {
+        _model = GenerativeModel(
+          model: 'gemini-1.5-flash', // Kiểm tra model hợp lệ
+          apiKey: apiKey,
+        );
+        _chatSession = _model.startChat();
+      });
+    } else {
+      print("⚠ API Key không hợp lệ hoặc rỗng");
+      }
+    } catch (e) {
+      print("❌ Lỗi khi đọc API Key: $e");
+    }
 
-  /// This is the callback that the SpeechToText plugin calls when
-  /// the platform returns recognized words.
-  void onSpeechResult(SpeechRecognitionResult result) {
-    setState(() {
-      lastWords = result.recognizedWords;
-    });
   }
 
   @override
   void dispose() {
-    super.dispose();
     speechToText.stop();
+    super.dispose();
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Aglie"),
         leading: const Icon(Icons.menu),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            //Virtual Assistants
-            Stack(
-              children: [
-                Center(
-                  child: Container(
-                    height: 120,
-                    width: 120,
-                    margin: const EdgeInsets.only(top: 4),
-                    decoration: const BoxDecoration(
-                      color: Pallete.assistantCircleColor,
-                      shape: BoxShape.circle
+            Expanded(
+              child: ListView.builder(
+                controller: _scrollController,
+                itemCount: _chatSession.history.length,
+                itemBuilder: (context, index) {
+                  final Content content = _chatSession.history.toList()[index];
+                  final text = content.parts
+                      .whereType<TextPart>()
+                      .map<String>((e) => e.text)
+                      .join('');
+                  return Message(
+                    text: text,
+                    isFromUser: content.role == 'user',
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: textFieldDecoration(),
+                      controller: _textController,
+                      onSubmitted: _sendChatMessage,
                     ),
                   ),
-                ),
-                Container(
-                  height: 123,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(image: AssetImage("assets/images/virtualAssistant.png"))
+                  IconButton(
+                    icon: const Icon(Icons.send, color: Colors.blue),
+                    onPressed: () {
+                      
+                      if (_textController.text.trim().isNotEmpty) {
+                        _sendChatMessage(_textController.text.trim());
+                      }
+                    },
                   ),
-                )
-              ],
-            ),
-        
-            //chat bubble
-            Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: 10,
-              ),
-              margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 30).copyWith(
-                top: 30,
-        
-              ),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Pallete.borderColor
-                ),
-                borderRadius: BorderRadius.circular(20).copyWith(
-                  topLeft: Radius.zero,
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10.0),
-                child: const Text('Hi! What task can I do for you?', 
-                  style: TextStyle(
-                    fontFamily: 'Cera Pro',
-                    color: Pallete.mainFontColor,
-                    fontSize: 25,
-                  ),
-                ),
+                ],
               ),
             ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              alignment: Alignment.centerLeft,
-              margin: const EdgeInsets.only(
-                top: 10,
-                left: 22,
-              ),
-              child: const Text('Here are a few features', style: TextStyle(
-                fontFamily: 'Cera Pro',
-                color: Pallete.mainFontColor,
-                fontSize: 20,
-                fontWeight: FontWeight.bold
-              ),),
-            ),
-            //Features List
-            Column(
-              children: [
-                FeatureBox(
-                  color: Pallete.firstSuggestionBoxColor,
-                  headerText: 'Chat GPT',
-                  descriptionText: 'If you dont change it, it mean you accept it',
-                ),
-                FeatureBox(
-                  color: Pallete.secondSuggestionBoxColor,
-                  headerText: 'Dall-E',
-                  descriptionText: 'Get inspired and stay creative with your personal assiatant powered by dall-e',
-                ),
-                FeatureBox(
-                  color: Pallete.thirdSuggestionBoxColor,
-                  headerText: 'Small Voice Assistant',
-                  descriptionText: 'Get the best of both worlds with a voice assiatant powered by Dall-E and chat GPT',
-                ),
-              ],
-            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Pallete.firstSuggestionBoxColor,
-        onPressed: () async {
-          if(await speechToText.hasPermission && speechToText.isNotListening){
-            startListening();
-          }
-          else if(speechToText.isListening){
-            stopListening();
-          }
-          else{
-            initSpeechToText();
-          }
-        },
-        child: const Icon(Icons.mic),
-      ),
-        
     );
   }
-  
+
+  InputDecoration textFieldDecoration() {
+    return InputDecoration(
+      contentPadding: const EdgeInsets.all(15),
+      hintText: 'Enter a prompt...',
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(14),
+      ),
+    );
+  }
+
+  Future<void> _sendChatMessage(String message) async {
+    setState(() {
+      _loading = true;
+    });
+
+    final response = await _chatSession.sendMessage(Content.text(message));
+    final text = response.text;
+
+    setState(() {
+      _loading = false;
+      _textController.clear();
+      _scrollDown();
+    });
+  }
+
+  void _scrollDown() {
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) => _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 750),
+        curve: Curves.easeOutCirc,
+      ),
+    );
+  }
 }
